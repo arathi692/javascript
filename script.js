28a9bd7b6c94f94c0ee76f3884a7aa0a779bbc8f
arathi <arathiprakash2001@gmail.com>
2023-11-29 20:53:30 +0530

    done

--- script.js
+++ script.js
@@ -863,10 +863,33 @@ console.log("factorial: ",fact)
     let myForm = document.getElementById('myForm');
     console.log("form:",myForm);
 
-    myForm.addEventListener('submit',function(e){
+     myForm.addEventListener('submit',function(e){
         e.preventDefault();//stops default behaviour form
 
         var datas={};
+
+        let firstname = document.getElementById('firstname');
+        let firstname_value=firstname.value;
+        console.log("firstname :",firstname_value);
+
+        let lastname = document.getElementById('lastname');
+        let  lastname_value=lastname.value;
+        console.log("lastname :",lastname_value);
+
+
+        let place = document.getElementById('place');
+        let place_value=place.value;
+        console.log("place :",place_value);
+
+        let phone = document.getElementById('phone');
+        let phone_value=phone.value;
+        console.log("phone :",phone_value);
+
+        let age = document.getElementById('age');
+        let age_value=age.value;
+        console.log("phone :",age_value);
+
+
 
         let email = document.getElementById('email');
         let email_value=email.value;
@@ -876,9 +899,13 @@ console.log("factorial: ",fact)
         let password=document.getElementById('password');
         let password_value=password.value;
         console.log("password:",password_value);
-
+        datas.firstname = firstname_value;
+        datas.lastname = lastname_value;
+        datas.place = place_value;
+        datas.phone = phone_value;
         datas.email =email_value;
         datas.password=password_value;
+        datas.age= age_value;
 
         console.log("datas:",datas);
     })
